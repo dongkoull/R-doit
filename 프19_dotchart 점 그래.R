dbf509da15ebe3c18d4674d2c0e7f227abdf9738
dongkoull <41326435+dongkoull@users.noreply.github.com>
2018-08-01 19:23:01 +0900

    Add files via upload

---
+++ 프19_dotchart 점 그래.R
@@ -0,0 +1,14 @@
+plot(mtcars$mpg)
+
+dotchart(mtcars$mpg,labels = row.names(mtcars),cex = 0.8) #cex = fontsize
+
+carmpg <- mtcars[order(mtcars$mpg),]
+carmpg$cyl <-factor(carmpg$cyl)
+class(carmpg$cyl)
+carmpg$color[carmpg$cyl==4] <-"blue"
+carmpg$color[carmpg$cyl==6] <- "green"
+carmpg$color[carmpg$cyl==8] <- "red"
+
+dotchart(carmpg$mpg,labels = row.names(carmpg),
+         groups=carmpg$cyl,cex = 0.8,
+         xlab="Miles Per Gallon",color=carmpg$color,main="Milage depend on the numbers of cylinder")
